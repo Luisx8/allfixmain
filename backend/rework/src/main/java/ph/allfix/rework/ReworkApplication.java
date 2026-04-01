02fd274e422668740f5f5c653d61b76f31ed80f4
Aeron <bulataoaeronjay@gmail.com>
2026-04-01 14:01:27 +0800

    add backend boilerplate springboot with dependencies

---
+++ backend/rework/src/main/java/ph/allfix/rework/ReworkApplication.java
@@ -0,0 +1,13 @@
+package ph.allfix.rework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReworkApplication.class, args);
+	}
+
+}
---
+++ backend/rework/src/test/java/ph/allfix/rework/ReworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.allfix.rework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
