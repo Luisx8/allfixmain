613bd840b167ece96069e0a8a11913b1bf873bc6
Aeron <bulataoaeronjay@gmail.com>
2026-04-06 16:34:54 +0800

    edit aws-config

--- frontend/allfix-frontend/src/aws-config.js
+++ frontend/allfix-frontend/src/aws-config.js
@@ -2,9 +2,9 @@ const awsConfig = {
   Auth: {
     Cognito: {
       //  Amazon Cognito User Pool ID
-      userPoolId: 'XX-XXXX-X_abcd1234',
+      userPoolId: 'ap-southeast-2_p7mounHZf',
       // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-      userPoolClientId: 'a1b2c3d4e5f6g7h8i9j0k1l2m3',
+      userPoolClientId: '7lv1c4r14gnr5he18r9vg70veg',
       // REQUIRED only if you use the identity pool
       // identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
       // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
